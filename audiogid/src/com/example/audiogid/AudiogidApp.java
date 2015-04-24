55382e6af4bbecf0cce5ec070330a2c6a22c05aa
Anya Kaygorodova <anutakay@gmail.com>
2015-04-25 00:26:34 +0700

    Сохранение текущей активности.

--- audiogid/src/com/example/audiogid/AudiogidApp.java
+++ audiogid/src/com/example/audiogid/AudiogidApp.java
@@ -1,7 +1,22 @@
 package com.example.audiogid;
 
+import android.app.Activity;
 import android.app.Application;
 
-public class AudiogidApp extends Application {
+public class AudiogidApp extends Application { 
+	
+	private Activity mCurrentActivity = null;
+	
+	public void onCreate() {
+        super.onCreate();
+	}
+ 
+	public Activity getCurrentActivity()	{
+		return mCurrentActivity;
+	}
+	
+	public void setCurrentActivity(final Activity mCurrentActivity) {
+        this.mCurrentActivity = mCurrentActivity;
+	}
 
 }
